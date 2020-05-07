b8dfccf9e93ca8e9a03863cdd5711c67bdee3c17
BuildTools <unconfigured@null.spigotmc.org>
2020-05-07 15:47:47 -0700

    using flask_restful for cleaner api

--- resources/movie.py
+++ resources/movie.py
@@ -1,32 +1,28 @@
-from flask import Blueprint, Response, request
+from flask import Response, request
 from database.models import Movie
+from flask_restful import Resource
 
-movies = Blueprint('movies', __name__)
+class MoviesApi(Resource):
+	def get(self):
+		movies = Movie.objects().to_json()
+		return Response(movies, mimetype="application/json", status=200)
 
-@movies.route('/movies')
-def get_movies():
-	movies = Movie.objects().to_json()
-	return Response(movies, mimetype="application/json", status=200)
+	def post(self):
+		body = request.get_json()
+		movie = Movie(**body).save()
+		id = movie.id
+		return {'id': str(id)}, 200
 
-@movies.route('/movies', methods=['POST'])
-def add_movie():
-	body = request.get_json()
-	movie = Movie(**body).save()
-	id = movie.id
-	return {'id': str(id)}, 200
+class MovieApi(Resource):
+	def put(self, id):
+		body = request.get_json()
+		Movie.objects.get(id=id).update(**body)
+		return '', 200
 
-@movies.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-	body = request.get_json()
-	Movie.objects.get(id=id).update(**body)
-	return '', 200
+	def delete(self, id):
+		movie = Movie.objects.get(id=id).delete()
+		return '', 200
 
-@movies.route('/movies/<id>', methods=['DELETE'])
-def delete_movie(id):
-	movie = Movie.objects.get(id=id).delete()
-	return '', 200
-
-@movies.route('/movies/<id>')
-def get_movie(id):
-	movies = Movie.objects.get(id=id).to_json()
-	return Response(movies, mimetype="application/json", status=200)
+	def get(self, id):
+		movie = Movie.objects.get(id=id).to_json()
+		return Response(movie, mimetype="application/json", status=200)
