2f4d8b5705cb2465c09a56fa81a20526b2e64a1b
BuildTools <unconfigured@null.spigotmc.org>
2020-05-07 19:19:29 -0700

    added auth and protected routes

--- resources/movie.py
+++ resources/movie.py
@@ -1,12 +1,15 @@
 from flask import Response, request
+from flask_jwt_extended import jwt_required
 from database.models import Movie
 from flask_restful import Resource
 
 class MoviesApi(Resource):
+	@jwt_required
 	def get(self):
 		movies = Movie.objects().to_json()
 		return Response(movies, mimetype="application/json", status=200)
 
+	@jwt_required
 	def post(self):
 		body = request.get_json()
 		movie = Movie(**body).save()
@@ -14,15 +17,17 @@ class MoviesApi(Resource):
 		return {'id': str(id)}, 200
 
 class MovieApi(Resource):
+	@jwt_required
 	def put(self, id):
 		body = request.get_json()
 		Movie.objects.get(id=id).update(**body)
 		return '', 200
 
+	@jwt_required
 	def delete(self, id):
 		movie = Movie.objects.get(id=id).delete()
 		return '', 200
-
+	@jwt_required
 	def get(self, id):
 		movie = Movie.objects.get(id=id).to_json()
 		return Response(movie, mimetype="application/json", status=200)
